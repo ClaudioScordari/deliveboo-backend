91673f22a7224995d98996d3c297668f3ab85815
gabrielebottari <gabriele.bottari1@gmail.com>
2024-03-29 23:28:17 +0100

    ADD favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
-    <link rel="icon" type="image/png" href="/favicon.ico">
+    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="image/png">
 
     <title>@yield('page-title') | {{ config('app.name', 'Laravel') }}</title>
 
