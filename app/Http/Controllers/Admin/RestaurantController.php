ced21ca016033728f5ef0a8dfcf263bda05a9faa
gabrielebottari <gabriele.bottari1@gmail.com>
2024-04-02 19:07:27 +0200

    FIX minor issues

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -157,7 +157,7 @@ class RestaurantController extends Controller
             $restaurant->types()->detach();
         }
 
-        return redirect()->route('admin.restaurants.show', compact('restaurant'));
+        return redirect()->route('admin.restaurants.index', compact('restaurant'));
         
     }
 
